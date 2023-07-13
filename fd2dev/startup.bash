99eefb4ca8d152e9b7d3931d34510f14fe6445fc
braughtg <braught@dickinson.edu>
2023-07-12 21:27:33 -0400

    Moved fd2grp creation to startup.bash

--- fd2dev/startup.bash
+++ fd2dev/startup.bash
@@ -2,28 +2,38 @@
 
 # Cleanup from past vnc session that may hav been running in the container.
 # This clean-up is not done when the container is stopped (not deleted).
-rm /tmp/.X11-unix/X1
-rm /tmp/.X1-lock
+rm /tmp/.X11-unix/X1 2> /dev/null
+rm /tmp/.X1-lock 2> /dev/null
 
-# Ensure that the fd2dev user is in a group that has RW permission to
-# the docker.sock file.  This will allow fd2dev to use the docker on docker
-# to interact with containers within the development environment.
-# Note: This must be here instead of in Dockerfile so the GID for the
-# docker.sock in the container can match the one on the host.
-# Note: The docker.gid file is mounted into the container by docker-compose.
-HOST_DOCKER_GID=$(cat ~/.contconf/gids/docker.gid)
+# Get the GID of the docker group on the host os. 
+# If a group with that GID already exists in the container use it.
+# Otherwise create a new fd2docker group with that GID in the container.
+HOST_DOCKER_GID=$(cat ~/.fd2/gids/docker.gid)
 HOST_DOCKER_GID_IN_CONTAINER=$(echo /etc/group | grep ":$HOST_DOCKER_GID:")
 if [ -z "$HOST_DOCKER_GID_IN_CONTAINER" ];
 then
   echo "fd2dev" | sudo -S groupadd --gid $HOST_DOCKER_GID fd2docker
 fi
+
+# Ensure that the fd2dev user can rwx /var/run/docker.sock
+# in the container.
 echo "fd2dev" | sudo -S usermod -a -G $HOST_DOCKER_GID fd2dev
 echo "fd2dev" | sudo -S chgrp +$HOST_DOCKER_GID /var/run/docker.sock
-echo "fd2dev" | sudo -S chmod 775 /var/run/docker.sock
+echo "fd2dev" | sudo -S chmod g+rwx /var/run/docker.sock
 
-# Ensure that the fd2grp has RW access to mounted FarmData2 directory.
-# This probably is not necessary as in linux the group will reflect
-# the host group.  But it does make things consistent across macOS and linux.
+# Get the GID of the fd2grp on the host os.
+# If a group with tht GID already exists in the container use it.
+# Otherwise create a new fd2grp group with that GID in the container.
+HOST_FD2GRP_GID=$(cat ~/.fd2/gids/fd2grp.gid)
+HOST_FD2GRP_GID_IN_CONTAINER=$(echo /etc/group | grep ":$HOST_FD2GRP_GID:")
+if [ -z "$HOST_FD2GRP_GID_IN_CONTAINER" ];
+then
+  echo "fd2dev" | sudo -S groupadd --gid $HOST_FD2GRP_GID fd2grp
+fi
+
+# Ensure that the fd2dev user has rw access to all of the stuff
+# in the FarmData2 repo via the fd2grp group.
+echo "fd2dev" | sudo -S usermod -a -G $HOST_FD2GRP_GID fd2dev
 echo "fd2dev" | sudo -S chgrp -R fd2grp /home/fd2dev/FarmData2
 echo "fd2dev" | sudo -S chmod -R g+rw /home/fd2dev/FarmData2
 
@@ -38,14 +48,5 @@ vncserver \
 
 # Launch the noVNC server.
 /usr/share/novnc/utils/launch.sh --vnc localhost:5901 --listen 6901 &
-
-# Check if the .contconf/launch.bash script exists and if it does
-# then run it here.  This allows images that use this as a base
-# to insert a script that will run on startup.
-if [ -f /home/fd2dev/.contconf/launch.bash ];
-then
-  echo "Running launch.bash"
-  source /home/fd2dev/.contconf/launch.bash
-fi
 
 sleep infinity
