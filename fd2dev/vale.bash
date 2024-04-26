a4b6d281c2a6302e42d6240ebead98a5a14702c9
Grant Braught <braught@dickinson.edu>
2024-04-26 15:37:05 -0400

    feat: install the vale linter

---
+++ fd2dev/vale.bash
@@ -0,0 +1,17 @@
+cd /var/tmp
+
+ARCH=$(dpkg --print-architecture)
+VER="3.4.1"
+
+if [ "$ARCH" = "arm64" ]; then
+    VALE_FILE=vale_"$VER"_Linux_arm64.tar.gz
+else
+    VALE_FILE=vale_"$VER"_Linux_64-bit.tar.gz 
+fi
+
+wget https://github.com/errata-ai/vale/releases/download/v"$VER"/"$VALE_FILE"
+mkdir "vale"
+tar -xvzf "$VALE_FILE" -C vale
+cp vale/vale /usr/local/bin
+rm "$VALE_FILE"
+rm -rf vale
